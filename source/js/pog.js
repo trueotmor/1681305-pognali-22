cf4aaa7d5dd039fbb65f4f85c1c25920ca89425f
trueotmor <proskurin.sasha78@gmail.com>
2021-05-23 19:38:53 +0300

    на предзащиту

--- source/js/pog.js
+++ source/js/pog.js
@@ -15,7 +15,7 @@ let toogle_page_header_scroll = function() {
 };
 
 function throttle(fn, wait) {
- let timeoutID = null;
+  let timeoutID = null;
   return function() {
       if (!timeoutID) {
           clearTimeout(timeoutID);
@@ -30,3 +30,22 @@ function throttle(fn, wait) {
 toogle_page_header_scroll();
 
 window.addEventListener('scroll', throttle(toogle_page_header_scroll, 300));
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938784, 30.323250],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+      myPlacemark = new ymaps.Placemark([59.938784, 30.323250], {
+          hintContent: 'Pogнали'
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: 'img/map/map_marker.svg',
+          iconImageSize: [41, 41],
+          iconImageOffset: [0, 0]
+      });
+  myMap.geoObjects
+      .add(myPlacemark);
+});
